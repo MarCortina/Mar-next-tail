ceeace610059e125fbe2253aa8a252d8a66fd108
MarCortina <marmica24@hotmail.com>
2021-05-29 20:51:21 -0300

    filtros ok

--- components/Nav.js
+++ components/Nav.js
@@ -2,24 +2,26 @@ import requests from '../utils/requests';
 import { useRouter } from 'next/router'
 
 function Nav() {
-    return <nav>
-        <div className='flex px-10 sm:px-20 whitespace-nowrap 
+    const router = useRouter();
+    return (
+        <nav>
+            <div className='flex px-10 sm:px-20 whitespace-nowrap 
         space-x-10 sm:space-x-20 overflow-x-scroll scrollbar-hide'>
-            {Object.entries(requests).map(([key, { title, url }]) => (
-                <h2 key={key} 
-                onClick={()=>Router.push(`/?genre=${key}`)}
-                className='last:pr-24 cursor-pointer transition
+                {Object.entries(requests).map(([key, { title, url }]) => (
+                    <h2 key={key}
+                        onClick={() => router.push(`/?genre=${key}`)}
+                        className='last:pr-24 cursor-pointer transition
                  duration-100 transform hover:scale-125 hover:text-white
                 active:text-black'>{title}</h2>
 
-            ))}
-        </div>
-        <div className='absolute top-0 rigth-0 bg-gradient-to-l from-
+                ))}
+            </div>
+            <div className='absolute top-0 rigth-0 bg-gradient-to-l from-
         [#06202A] h-10 w-1/12'>
 
-        </div>
-    </nav>
-
+            </div>
+        </nav>
+    )
 }
 
 export default Nav
--- components/Thumbnail.js
+++ components/Thumbnail.js
@@ -4,23 +4,23 @@ import { forwardRef } from "react";
 
 const Thumbnail = forwardRef(({ result }, ref) => {
     const BASE_URL = "https://image.tmdb.org/t/p/original/";
+
     return (
-        <div ref={ref} className='group cursor-pointer transition duration-200
+        <div ref={ref} className='p-2 group cursor-pointer transition duration-200
         ease-in transform sm:hover:scale-x-105 hover:z-50'>
-            <Image 
-            layout='responsive'
-            src={
-                `${BASE_URL}${result.backdrop_path ||
-                result.poster_path}` ||
-                `${BASE_URL}${result.poster_path}`
-            }
-            height={1000}
-            width={1920}
+            <Image
+                layout='responsive'
+                src={
+                    `${BASE_URL}${result.backdrop_path || result.poster_path}` ||
+                    `${BASE_URL}${result.poster_path}`
+                }
+                height={1000}
+                width={1920}
             />
-            <div>
-                <p className='truncate max-w-md'>
-                   {result.overview} 
-                </p>
+
+            <div className='p-2'>
+                <p className='truncate max-w-md'> {result.overview} </p>
+
                 <h2 className='mt-1 text-2xl text-white transition-all
                 duration-100 ease-in-out group-hover:font-bold'>
                     {result.title || result.original_name}
